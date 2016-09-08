18d31fefc545b5c2f0f164153b1e199843813a70
raymondphua <chrphua@student.avans.nl>
2016-09-08 09:18:03 +0200

    start up

---
+++ home.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Raymond Phua
+ * Date: 8-9-2016
+ * Time: 09:16
+ */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Test</title>
+</head>
+<body>
+<?php echo "dit is ons pagina." ?>
+</body>
+</html>
